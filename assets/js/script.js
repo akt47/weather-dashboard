06c2d7e864aa617ae443a6fd2e979048d2bd4e9d
akt47 <78057009+akt47@users.noreply.github.com>
2021-05-19 23:16:15 -0500

    html finished

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function initPage () {
+const cityEl = document.getElementById("enter-city");
+const searchEl = document.getElementById("search-button");
+const clearEl = document.getElementById("clear-history");
+const nameEl = document.getElementById("city-name");
+const currentPicEl = document.getElementById("current-pic");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+const historyEl = document.getElementById("history");
+var fivedayEl = document.getElementById("fiveday-header");
+var todayweatherEl = document.getElementById("today-weather");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+
+
+// API key from open weather.com
+const APIKey = "8f42e7b2052fe7b99e8ea281a40f15ff";
+
+function getWeather(cityName){
+  //current weather API call key   
+
+}
+
+
+}
+
+initPage();
